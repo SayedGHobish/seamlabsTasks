8e686684d17c96cd8b997181bf3865f714fd5322
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:03:04 +0200

    Update PageBase.java

--- src/main/java/PageBase/PageBase.java
+++ src/main/java/PageBase/PageBase.java
@@ -1,2 +1,22 @@
-package PageBase;public class PageBase {
+package PageBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageBase {
+    protected WebDriver driver;
+    public Actions action;
+    public PageBase(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public  Actions act(WebElement element,WebElement locators)
+    {
+        Actions Hover = new Actions(driver);
+        Hover.moveToElement(element).moveToElement(locators).click().build().perform();
+        return Hover;
+    }
 }
