21bd55450bc39452a25de1ac7c0318ba9cab00b8
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:02:32 +0200

    Update HomePage.java

--- src/main/java/PageBase/HomePage.java
+++ src/main/java/PageBase/HomePage.java
@@ -1,2 +1,23 @@
-package PageBase;public class HomePage {
+package PageBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends PageBase{
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        action = new Actions(driver);
+    }
+    @FindBy(xpath = "//*[@id=\"HEADER_MENU_BAR_CATEGORY_0\"]/div/a")
+    public WebElement FurnitureIcon;
+
+    @FindBy(linkText = "Outdoors")
+    public WebElement outdoorsItems;
+//    public void selectOutDoorsItems()
+//    {
+//        action.moveToElement(FurnitureIcon).moveToElement(outdoorsItems).click().perform();
+//    }
 }
