7917b37ce0c4adc63f2147de21fbab167991c29f
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:02:17 +0200

    Update Checkout.java

--- src/main/java/PageBase/Checkout.java
+++ src/main/java/PageBase/Checkout.java
@@ -1,2 +1,26 @@
-package PageBase;public class Checkout {
+package PageBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Checkout extends PageBase{
+    public Checkout(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id ="HEADER_CART_BRIEF_ICON")
+    public WebElement  check;
+
+    @FindBy(id ="HEADER_CART_BRIEF_GO_TO_CART_BUTTON")
+    public WebElement  goToCart;
+
+    @FindBy(xpath = "//*[@id=\"app-root\"]/div[2]/div[2]/div/div[1]/div[2]/div/div[3]/span[2]/b")
+    public WebElement total;
+
+    public void checks()
+    {
+        check.click();
+        goToCart.click();
+    }
 }
