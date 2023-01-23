6af88f7e2b113cd731ae3513906d9840203d315f
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:03:57 +0200

    Update TestCases.java

--- src/test/java/SelenuimTest/TestCases.java
+++ src/test/java/SelenuimTest/TestCases.java
@@ -1,2 +1,46 @@
-package SelenuimTest;public class TestCases {
+package SelenuimTest;
+
+import PageBase.HomePage;
+import PageBase.ItemsPage;
+import PageBase.PageBase;
+import org.testng.Assert;
+import PageBase.Checkout;
+import org.testng.annotations.Test;
+
+public class TestCases extends TestBais {
+    PageBase baseObj;
+    HomePage homeObj;
+    ItemsPage itemsobj;
+    Checkout checkObj;
+
+    @Test
+    public void navigateOutDoors()
+    {
+        baseObj = new PageBase(driver);
+        homeObj = new HomePage(driver);
+        baseObj.act(homeObj.FurnitureIcon, homeObj.outdoorsItems);
+        String currentUrls =  driver.getCurrentUrl();
+        Assert.assertEquals(currentUrls,"https://homzmart.com/en/c/outdoors?page=1");
+    }
+
+    @Test
+    public void openSecondOrder()
+    {
+        itemsobj = new ItemsPage(driver);
+        itemsobj.secendOrder.click();
+
+    }
+
+    @Test
+    public void checkOut()
+    {
+        checkObj = new Checkout(driver);
+        itemsobj = new ItemsPage(driver);
+        String productPricesText = itemsobj.prouductPrices.getText();
+        itemsobj.addToCart.click();
+        checkObj.checks();
+        String totalPrices = checkObj.total.getText();
+        Assert.assertEquals(totalPrices,productPricesText);
+    }
+
 }
