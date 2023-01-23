c5c83ae75ca3ef8d6574b25102d5878704e550d4
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:03:33 +0200

    Update TestBais.java

--- src/test/java/SelenuimTest/TestBais.java
+++ src/test/java/SelenuimTest/TestBais.java
@@ -1,2 +1,27 @@
-package SelenuimTest;public class TestBais {
+package SelenuimTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBais {
+    WebDriver driver;
+
+    @BeforeTest
+    public void openURL()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://homzmart.com/en/");
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void closeURL()
+    {driver.quit();}
 }
