4f8030d8947d29ad95e9b31230a629e712ae837b
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:04:52 +0200

    Update TestRunner.java

--- src/test/java/BDD/Runner/TestRunner.java
+++ src/test/java/BDD/Runner/TestRunner.java
@@ -1,4 +1,11 @@
 package BDD.Runner;
 
-public class TestRunner {
+import BDD.Runner.TestBase;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(features = "src/test/java/BDD/Features",
+                glue = ("BDD/Steps"),
+                plugin = {"pretty","html:target/cucumber-html-report.html"})
+public class TestRunner extends TestBase {
 }
