da99ae05a108a980897408e6d4970d96b601fa66
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:04:36 +0200

    Update TestBase.java

--- src/test/java/BDD/Runner/TestBase.java
+++ src/test/java/BDD/Runner/TestBase.java
@@ -1,2 +1,27 @@
-package BDD.Runner;public class TestBase {
+package BDD.Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase extends AbstractTestNGCucumberTests {
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void openURL()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://homzmart.com/en/");
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void closeURL()
+    {driver.quit();}
 }
