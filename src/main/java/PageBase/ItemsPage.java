d86d5eeeb1673b4a9b8c622854536ce2e585559c
Sayed Salah El-Din <107331333+SayedGHobish@users.noreply.github.com>
2023-01-23 12:02:47 +0200

    Update ItemsPage.java

--- src/main/java/PageBase/ItemsPage.java
+++ src/main/java/PageBase/ItemsPage.java
@@ -1,2 +1,21 @@
-package PageBase;public class ItemsPage {
+package PageBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ItemsPage extends PageBase{
+    public ItemsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id=\"app-root\"]/div[2]/div[2]/div/div[1]/div[2]/div[2]/div[2]/div/a")
+    public WebElement secendOrder;
+    @FindBy(xpath = "//*[@id=\"app-root\"]/div[2]/div[2]/div/section/div[2]/div[1]/div[2]/div/h3/div")
+     public WebElement prouductPrices;
+
+    @FindBy(className = "button-with-icon second-bg sm")
+    public WebElement addToCart;
+
+
 }
